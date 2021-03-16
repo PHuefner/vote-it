96a0dc331d17947f44c3050214bb4a27e4068c37
Marcel Kucera <marcelkucera@gmx.de>
2021-03-16 21:57:25 +0100

    Added Set-Cookie functionality in response

--- backend/src/main/java/voteit/libs/serverhttp/Response.java
+++ backend/src/main/java/voteit/libs/serverhttp/Response.java
@@ -1,5 +1,7 @@
 package voteit.libs.serverhttp;
 
+import java.util.HashMap;
+
 import voteit.libs.json.JsonArray;
 import voteit.libs.json.JsonObject;
 
@@ -7,13 +9,8 @@ public class Response {
 
     int status;
     String response;
+    HashMap<String, String> headers = new HashMap<>();
     String contentType;
-
-    public Response(int status, String response) {
-        this.status = status;
-        this.response = response;
-        this.contentType = "text/plain";
-    }
 
     public Response(String stringResponse) {
         this.status = 200;
@@ -33,10 +30,23 @@ public class Response {
         this.contentType = "application/json";
     }
 
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void addCookie(String key, String value) {
+        headers.put(key, value);
+    }
+
     public String build() {
         StringBuilder out = new StringBuilder();
         out.append("HTTP/1.0 " + status + "\n");
         out.append("Content-Type: " + contentType + "; charset=UTF-8" + "\n");
+        if (!headers.isEmpty()) {
+            headers.forEach((k, v) -> {
+                out.append(String.format("Set-Cookie: %s=%s;\n", k, v));
+            });
+        }
         out.append("\n");
         out.append(response);
         out.append("\n");
