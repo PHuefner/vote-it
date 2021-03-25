c3cd23f1b9d2dea553baad08eb62d43c6581ac51
Paul Hüfner <p.huefner03@gmail.com>
2021-03-25 18:51:28 +0100

    Added cookiehandling for get-requests

--- backend/src/main/java/voteit/handlers/GetHandler.java
+++ backend/src/main/java/voteit/handlers/GetHandler.java
@@ -22,16 +22,18 @@ public class GetHandler implements Handler {
                 return null;
             }
         } else if (context.route.contains("user/data")) {
+            String id = context.cookies.get("userId");
             try {
-                return new Response(Manager.getUserJson(1));
+                return new Response(Manager.getUserJson(Integer.parseInt(id)));
             } catch (SQLException e) {
                 System.out.println("Couldn't create UserObject.");
                 System.out.println(e.getMessage());
                 return null;
             }
         } else if (context.route.contains("poll/get")) {
+            String id = context.cookies.get("pollId");
             try {
-                return new Response(Manager.getPollJson(1));
+                return new Response(Manager.getPollJson(Integer.parseInt(id)));
             } catch (SQLException e) {
                 System.out.println("Couldn't create PollObject");
                 System.out.println(e.getMessage());
